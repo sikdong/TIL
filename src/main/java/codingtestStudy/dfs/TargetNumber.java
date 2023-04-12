0b3d8641f354863301817c098e876e6c30db12a2
sikdong <zmstnv1119@naver.com>
2023-04-12 14:34:55 +0900

    프로그래머스 '타겟 넘버' dfs 공부

---
+++ src/main/java/codingtestStudy/dfs/TargetNumber.java
@@ -0,0 +1,34 @@
+package codingtestStudy.dfs;
+
+public class TargetNumber {
+    int count = 0;
+
+
+    public static void main(String[] args) {
+        TargetNumber test = new TargetNumber();
+        int[] numbers = {1, 1, 1, 1};
+        int target = 3;
+        System.out.println(test.solution(numbers, 3));
+    }
+
+    public int solution(int[] numbers, int target) {
+
+
+        dfs(numbers, 0, 4, target, 0);
+        System.out.println(count);
+        return count;
+
+    }
+        public void dfs( int[] numbers, int depth, int length, int target, int sum){
+            if (depth == length) {
+                if (sum == target) {
+                    count++;
+                    System.out.println(count);
+                }
+            } else {
+                dfs(numbers, depth + 1, length, target, sum + numbers[depth]);
+                dfs(numbers, depth + 1, length, target, sum - numbers[depth]);
+            }
+        }
+}
+
