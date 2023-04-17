1bc3814439feff062c490461ab38fac99dbf97b2
sikdong <zmstnv1119@naver.com>
2023-04-17 20:27:29 +0900

    프로그래머스 '게임 맵 최단거리' dfs/bfs 코드 수정

--- src/main/java/codingtestStudy/dfs/GameMap.java
+++ src/main/java/codingtestStudy/dfs/GameMap.java
@@ -1,75 +1,50 @@
 package codingtestStudy.dfs;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class GameMap {
-    int minimum = Integer.MAX_VALUE;
+    // 상하좌우 방향 표시
+    int[] dx = {-1, 1, 0, 0};
+    int[] dy = {0,0,-1, 1};
     int[][] maps = {{1,0,1,1,1},{1,0,1,0,1},{1,0,1,1,1},{1,1,1,0,1},{0,0,0,0,1}};
+
     public static void main(String[] args) {
-
         GameMap gm = new GameMap();
-        if(gm.maps[gm.maps.length-1][gm.maps[0].length-2] == 0 &&
-            gm.maps[gm.maps.length-2][gm.maps[0].length-1] == 0
-        ){
-            gm.minimum = -1;
-        }
+        Queue<int[]> q = new LinkedList<>();
+        int[][] visited = new int[gm.maps.length][gm.maps[0].length];
+        gm.bfs(q, visited);
+        int answer = visited[gm.maps.length-1][gm.maps[0].length-1];
+        System.out.println(answer);
 
-        List<Integer> first = gm.down(gm.maps, 0);
-        List<Integer> second = gm.side(gm.maps, first.get(0), first.get(1), first.get(2));
 
     }
 
-    List<Integer> down(int[][] maps, int sum){
-        List<Integer> room = new ArrayList<>();
-        int i;
-        for(i =0; i <maps.length; i++){
-            if(maps[i][0] != 0 && i < maps.length){
-                sum+=maps[i][0];
+    void bfs(Queue<int[]> q, int[][] visited){
+        //큐에 현재 좌표 노드 넣고 표시
+        q.add(new int[]{0,0});
+        while(!q.isEmpty()){
+            int[] now = q.poll();
+            int x = now[0];
+            int y = now[1];
+            visited[0][0] = 1;
+
+            //상하좌우 탐색
+            for(int i = 0; i < 4; i++){
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if(nx >= 0 && nx < maps.length && ny >=0 && ny < maps[0].length
+                        && visited[nx][ny] ==0 &&maps[nx][ny] != 0 ){
+                    visited[nx][ny] = visited[x][y] + 1;
+                    q.add(new int[]{nx, ny});
+                }
             }
         }
-        room.add(0,sum);
-        if(i != 0){
-            room.add(1, i-2);
-        } else {
-            room.add(1, i);
-        }
-        room.add(2, 0);
-        System.out.println("room "+ room);
-        return room;
     }
-
-    List<Integer> side(int[][] maps, int sum, int s, int e){
-        List<Integer> room = new ArrayList<>();
-        int i;
-        for(i = e+1; i < maps[0].length; i++){
-            if(maps[s][i] != 0 && i < maps[0].length){
-                sum+=maps[s][e];
-            } else {
-                break;
-            }
-        }
-        room.add(0, sum);
-        room.add(1, s);
-        if(i != 0){
-            room.add(2, i-1);
-        }else {
-            room.add(2, 0);
-        }
-        System.out.println("room "+ room);
-        return room;
-    }
-
- /*   List<Integer> up(int[][] maps, int sum, int s, int e){
-        List<Integer> room = new ArrayList<>();
-        int i;
-        for(i = s-1; i >= 0; i--){
-            if()
-        }
-
-    }
-*/
-
-
-
 }
+
+
+
