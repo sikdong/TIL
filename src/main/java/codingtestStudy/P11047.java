b2d18b4dbe7e815ba1c5c4fa387e46bb0dd8de80
sikdong <zmstnv1119@naver.com>
2023-04-25 11:50:29 +0900

    백준 greedy/우선순위 큐 공부

--- src/main/java/codingtestStudy/P11047.java
+++ src/main/java/codingtestStudy/P11047.java
@@ -9,21 +9,19 @@ public class P11047 {
         int goal = sc.nextInt();
         int[] coins = new int[coinKind];
 
+        //입력된 동전 값 배열에 넣기
         for(int i = 0; i < coinKind; i++){
             coins[i] = sc.nextInt();
         }
-
+        //동전 개수
         int quantity = 0;
             for(int i = coins.length-1; i >= 0; i--){
-                if(coins[i] <= goal){
+                //목표 금액보다 같거나 작은 경우 coins[i]가 무엇인지 확인
+                if(coins[i] <= goal) {
                     quantity += goal / coins[i];
                     goal -= (goal / coins[i]) * coins[i];
-                    System.out.println(goal);
-                }
-                if(goal == 0){
-                    System.out.println(quantity);
-                    break;
                 }
             }
+        System.out.println(quantity);
     }
 }
