50dcd59bf0afc96aa259251a98a7716f0f10845c
sikdong <zmstnv1119@naver.com>
2023-04-25 11:50:38 +0900

    백준 greedy/우선순위 큐 공부

--- src/main/java/codingtestStudy/P1715.java
+++ src/main/java/codingtestStudy/P1715.java
@@ -1,6 +1,5 @@
 package codingtestStudy;
 
-import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
@@ -9,13 +8,15 @@ public class P1715 {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
         PriorityQueue<Long> pq = new PriorityQueue<>();
+
         for(int i = 0; i < number; i++){
             pq.add(sc.nextLong());
         }
+        //int 범위가 넘을 수 있으니 long으로
         long first;
         long second;
         long sum = 0;
-
+            //우선순위 큐에 데이터가 1개 남을 때 까지
             while(pq.size() > 1){
                 first = pq.poll();
                 second = pq.poll();
